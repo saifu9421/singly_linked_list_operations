1d8e9c98319ca6d130cce6f066e88f3255d144b7
saifu9421 <saifursaif905@gmail.com>
2024-11-01 03:35:17 +0600

    sorted-linked-list

---
+++ sroted_linked_list.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+    class Node{
+      public:
+         int val;
+          Node *next;
+            Node(int val){
+                this->val =  val;
+                  this->next = NULL;
+            }
+    };
+     void insert(Node *&head,int val){
+           Node * newNode =  new Node(val);
+             if(head == NULL){
+                  head =  newNode;
+                   return;
+             };
+              Node *tmp = head;
+                while (tmp->next != NULL)
+                {
+                      tmp =  tmp->next;
+                };
+                 tmp->next = newNode;
+     };
+       
+       int size(Node *head){
+          Node *tmp =  head; 
+       int count = 0;
+        while (tmp != NULL)
+        {
+             count++;
+             tmp =  tmp->next;
+        };
+           return count;
+       };
+        
+        void sorted(Node *head){
+               Node *tmp = head;
+                 bool flag =  true;
+                for(int i = 1; i<=size(head);i++){
+                     Node *tmp2 =  tmp->next;
+                    for(int j =  i+1; j<= size(head); j++){
+                            if(tmp->val >  tmp2->val){
+                                   flag  = false;
+                                   break;
+                            };
+                            tmp2 = tmp2->next;
+                    };
+                      tmp = tmp->next;
+                };
+
+                 if(flag == true){
+                     cout<<"Yes"<<endl;
+                 }else{
+                     cout<<"NO"<<endl;
+                 };
+
+        };
+         
+        
+ int main(){
+        Node *head = NULL;
+          while (true)
+          {
+             int val;
+              cin>>val; 
+               if(val == -1) break;
+               insert(head,val);
+          };
+           sorted(head);
+     return 0;
+ }
